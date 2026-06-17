3045f78f8fefd7262974bca66e870969422d289d
barrows15 <nayanashutosh@gmail.com>
2026-06-17 11:19:40 +0530

    feature2 changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1> Hello feature 1 changes </h1>
+        <h2> Hello feature 2 changes </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
